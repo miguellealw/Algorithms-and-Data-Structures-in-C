67392e0867c4e852543bed39278ddc46cf20f78d
Miguel Leal <miguellealw@gmail.com>
2020-11-30 15:21:23 -0600

    Added ID to list node

--- data_structures/linked_list.h
+++ data_structures/linked_list.h
@@ -3,17 +3,23 @@
 
 typedef struct Node
 {
+  unsigned int ID;
   char *some_string;
   char **double_pointer;
   struct Node *next;
 } Node;
 
 Node *add_node_end(Node *head, char *some_string, char **double_pointer);
-Node *add_node_in(Node *head, char *some_string, char **double_pointer);
+Node *add_node_in(Node *head, int index, char *some_string, char **double_pointer);
 Node *add_node_begin(Node *head, char *some_string, char **double_pointer);
 
-Node *delete_node(Node *head, char *some_string);
-Node *find_node(Node *head, char *some_string);
+Node *delete_node(Node *head, unsigned int ID);
+//Node *find_node(Node *head, char *some_string);
+Node *find_node(Node *head, unsigned int ID);
+
+Node *create_node(Node *head, char *some_string, char **double_pointer);
+
+unsigned int length(Node *head);
 
 void print_list(Node *head, char *message);
 
