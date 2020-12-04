ae4b2b3528c108eda1e1bba860527dfdaf80bf3e
Miguel <miguellealw@gmail.com>
2020-12-04 00:52:12 -0600

    Added main file for main function

---
+++ data_structures/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "linked_list.h"
+
+// make this run all the data structures tests in a CLI
+int main(void)
+{
+
+  printf("\nChoose which Data Structure Test to Run: \n"); 
+  printf("-----------------------------------\n"); 
+  printf("A) Singly Linked List\n");
+  printf("B) Doubly Linked List\n");
+  printf("C) Stack\n");
+  printf("D) Queue\n");
+
+  return EXIT_SUCCESS;
+}
